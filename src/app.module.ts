caaf29e3d2bd515ae31a99308abdf81412596892
Vachagan Grigoryan <vachagan.grigoryan@outlook.com>
2022-02-21 09:56:09 +0400

    Work on: - added configuration, - added mongoDB, - added tasks and redis configuration for queues

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+
 import { AccountModule } from './account/account.module';
 import { LoggerMiddleware } from './logger.middleware';
+import configuration from './config/configuration';
+import { TasksModule } from './tasks/tasks.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nft-finder'),
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    // MongooseModule.forRoot('mongodb://localhost:27017/nft-finder'),
+    MongooseModule.forRoot(`mongodb://localhost:27017/nft-finder`),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    ScheduleModule.forRoot(),
+    TasksModule,
     AccountModule,
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
